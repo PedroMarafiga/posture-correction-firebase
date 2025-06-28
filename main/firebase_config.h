afb49a7ef7a193dae2ef67b9d9a34a167e346b31
Gustavo Weber Denardin <gustavo.denardin@gmail.com>
2025-06-28 16:10:27 -0300

    remove firebase info.

--- main/firebase_config.h
+++ main/firebase_config.h
@@ -1,9 +1,9 @@
 // Read readme.md to properly configure api key and authentication
 
 // create a new api key and add it here 
-#define API_KEY "AIzaSyDZj7vxifNQVbb4ST79SwW7brSNjFFJ6pE"
+#define API_KEY "put_here_the_api_key_from_your_firebase_project"
 // Copy your firebase real time database link here 
-#define DATABASE_URL "https://se28cp-default-rtdb.firebaseio.com/"  
+#define DATABASE_URL "https://your-database-default-rtdb.firebaseio.com/"  
 
 #define USER_EMAIL "gustavo.denardin@gmail.com"   // This gmail does not exist outside your database. it only exists in the firebase project as a user
-#define USER_PASSWORD "123654abcd"      // Dont add your gmail credentials. Setup users authentication in your Firebase project first
+#define USER_PASSWORD "user_password"      // Dont add your gmail credentials. Setup users authentication in your Firebase project first
