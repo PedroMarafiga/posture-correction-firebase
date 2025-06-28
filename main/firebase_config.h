c1fc5eb461ea9c32cb61f4480a4bfacfd6b4e7ee
Gustavo Weber Denardin <gustavo.denardin@gmail.com>
2025-06-28 16:08:30 -0300

    first commit

---
+++ main/firebase_config.h
@@ -0,0 +1,9 @@
+// Read readme.md to properly configure api key and authentication
+
+// create a new api key and add it here 
+#define API_KEY "AIzaSyDZj7vxifNQVbb4ST79SwW7brSNjFFJ6pE"
+// Copy your firebase real time database link here 
+#define DATABASE_URL "https://se28cp-default-rtdb.firebaseio.com/"  
+
+#define USER_EMAIL "gustavo.denardin@gmail.com"   // This gmail does not exist outside your database. it only exists in the firebase project as a user
+#define USER_PASSWORD "123654abcd"      // Dont add your gmail credentials. Setup users authentication in your Firebase project first
